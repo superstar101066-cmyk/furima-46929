2b2983344ae041982024c3b5ffa6456b18522f97
としき <superstar101066@gmail.com>
2026-01-08 12:03:17 +0900

    トップページへのルートパスを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  #HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
+  get '/items', to: 'items#index' #一覧表示ページを表示する
 end
+
+#HTTPメソッド
+#get    情報取得
+#post   情報作成
+#patch  情報更新
+#put    情報更新
+#delete 情報削除
+
+#URIパターン
+#/items
+
+#コントローラー名
+#itemsコントローラー
+
+#アクション名
+#index   一覧表示ページを表示する
+#new     新規登録ページを表示する
+#create  新規登録の保存
+#show    詳細表示ページを表示する
+#edit    編集ページを表示する
+#update  編集の保存
+#destroy 削除
