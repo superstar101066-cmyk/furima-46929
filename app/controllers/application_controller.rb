0eaff939dd325c615c830ea8ca966ce52b5f3d89
としき <superstar101066@gmail.com>
2026-01-09 21:01:28 +0900

    Deviseの導入、ユーザーモデルの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  # Deviseのコントローラーが動くとき、パラメーター許可のメソッドを実行する
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +10,14 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters #パラメータ許可のメソッドを定義
+    # 新規登録時に、設計した追加カラムを許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 end
+
+    # devise_parameter_sanitizer	Devise専用の「門番（パラメーターをチェックする人）」
+    # permit	許可する
+    # :sign_up	新規登録のとき
+    # keys:[...]	許可したいカラム名のリスト
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'items#index' #一覧表示ページを表示する
 end
 
