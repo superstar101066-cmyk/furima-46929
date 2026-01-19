f2958b7e5fc21160474c82049ac993108a482648
としき <superstar101066@gmail.com>
2026-01-19 18:06:35 +0900

    各モデルのアソシエーション設定（User, Item, Order, Address）

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
-  belongs_to :order
+  # アソシエーション設定
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture  # 発送先住所情報と都道府県の関係は多対1
+
+  belongs_to :order # 発送先住所情報と購入した商品の関係は多対1
 end
