9bbad39b96c47586021194cbd102ffac18b4a782
としき <superstar101066@gmail.com>
2026-01-25 15:23:21 +0900

    NokogiriなどのGemインストールエラーを回避する設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+# NokogiriなどのGemインストールエラーを回避する設定
+set :bundle_config, { force_ruby_platform: true }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
